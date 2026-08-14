791571250e2dbb5f78e0cecbecda043dc0d49409
sandeepreddy46 <sandeepreddyo358@gmail.com>
2026-08-14 11:11:10 +0530

    day2

--- frontend/index.js
+++ frontend/index.js
@@ -1,1 +1,44 @@
-alert("Hello from san js")
+
+const users = [
+    {
+        "name" : "John banega don",
+        "gender" : "Male",
+        "image" : "john.png"
+    },
+    {
+        "name" : "Janregtre",
+        "gender" : "FeMale",
+        "image" : "jane.png"
+    }
+]
+var curIndex = 0;
+
+
+function toggle(){
+ if(curIndex == 0)
+   curIndex = 1;
+ else{
+   curIndex = 0;
+ }
+
+
+ document.getElementById("card-image").src = users[curIndex].image
+ document.getElementById("card-name").innerText =users[curIndex].name
+ document.getElementById("card-gender").innerText =users[curIndex].gender
+}
+function random() {
+    fetch('https://randomuser.me/api')
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(data) {
+            console.log(data)
+            var detail = data.results[0];
+
+            document.getElementById("card-image").src = detail.picture.large;
+            document.getElementById("card-gender").innerText = detail.gender;
+
+            var fullname = detail.name.first + " " + detail.name.last;
+            document.getElementById("card-name").innerText = fullname;
+        });
+}
